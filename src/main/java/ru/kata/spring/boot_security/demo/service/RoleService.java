f8e1876f4b55284ea338ae79e6447ab62e9986cd
AlexCoderRus <115412380+AlexCoderRus@users.noreply.github.com>
2023-01-16 21:32:55 +0300

    Initial commit

---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/RoleService.java
@@ -0,0 +1,27 @@
+package ru.kata.spring.boot_security.demo.service;
+
+
+import org.springframework.stereotype.Service;
+import ru.kata.spring.boot_security.demo.model.Role;
+import ru.kata.spring.boot_security.demo.repository.RoleRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class RoleService {
+
+    private final RoleRepository roleRepository;
+
+    public RoleService(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    public List<Role> findAllRoles() {
+        return roleRepository.findAll();
+    }
+
+    public List<Role> findRolesById(Long id) {
+        return roleRepository.findRoleById(id);
+    }
+}
